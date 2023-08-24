fcb25ba2166836ee51729f93a0b1f25a22078d5d
okanay <okanay@hotmail.com>
2023-08-24 23:46:17 +0300

    TicketPage v.1

--- libs/helpers/FormattedDate.tsx
+++ libs/helpers/FormattedDate.tsx
@@ -4,12 +4,14 @@ import { tr } from 'date-fns/locale'
 export function FormattedDate(isoFlightDate: string | undefined | Date) {
    let formattedDate = ''
    let dayName = ''
+   let formattedTime = ''
 
    if (isoFlightDate !== undefined) {
       const parsedDate = new Date(isoFlightDate)
       formattedDate = format(parsedDate, 'dd MMMM', { locale: tr })
       dayName = format(parsedDate, 'EEEE', { locale: tr })
+      formattedTime = format(parsedDate, 'HH:mm', { locale: tr })
    }
 
-   return { formattedDate, dayName }
+   return { formattedDate, dayName, formattedTime }
 }
