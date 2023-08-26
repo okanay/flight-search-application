6710ea41f6ca1773f4a6c4b82abe88a00544cffc
okanay <okanay@hotmail.com>
2023-08-26 18:10:40 +0300

    Fix error

--- src/components/UI/TicketSearchForm/TicketFormErrorHandle.tsx
+++ src/components/UI/TicketSearchForm/TicketFormErrorHandle.tsx
@@ -1,10 +1,18 @@
 'use client'
 
-import { useSelector } from 'react-redux'
-import { getTicketError, TTicketSearchFormError } from '../../../../redux/slices/TicketSearchFormErrorSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { clearError, getTicketError, TTicketSearchFormError } from '../../../../redux/slices/TicketSearchFormErrorSlice'
+import { useEffect } from 'react'
 
 export const TicketFormErrorHandle = () => {
    const { isError, message }: TTicketSearchFormError = useSelector(getTicketError)
+   const dispatch = useDispatch()
+
+   useEffect(() => {
+      return () => {
+         dispatch(clearError())
+      }
+   }, [dispatch])
 
    return isError ? (
       <p className={'mt-2 font-openSans text-[12px] font-semibold text-red-400 smTablet:text-[16px]'}>{message}</p>
