6e0a0f3288766d7ab08eaa9644ef7909fb3b1b52
okanay <okanay@hotmail.com>
2023-08-26 10:02:56 +0300

    Major classname error fix

--- src/components/UI/TicketSearchForm/TicketErrorHandle.tsx
+++ src/components/UI/TicketSearchForm/TicketErrorHandle.tsx
@@ -7,7 +7,7 @@ export const TicketErrorHandle = () => {
    const { isError, status, message }: TTicketSearchFormError = useSelector(getTicketError)
 
    return isError ? (
-      <p className={'mt-2 font-openSans text-[12px] font-semibold text-red-400 text-slate-800 smTablet:text-[16px]'}>{message}</p>
+      <p className={'mt-2 font-openSans text-[12px] font-semibold text-red-400 smTablet:text-[16px]'}>{message}</p>
    ) : (
       <></>
    )
